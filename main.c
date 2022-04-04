92a551233924eac833d4bf3f9a74622327e070e0
Daniel Briones <dfbrione@usc.edu>
2022-04-03 22:35:21 -0700

    Starter files
    Added lcd.h and lcd.c .. Might have to tweak both files, but something to start with

---
+++ main.c
@@ -0,0 +1,12 @@
+/********************************************
+ *  Team Members: Daniel Briones, Devin Rosenthal, Ryan Sim, Wenchao Wang
+ *  Smart Green House
+ ********************************************/
+
+#include lcd.h
+
+
+int main(void) {
+
+    return 0;
+}
