585ba4bcda01917a4b0c3ca8b4019e78711fd885
Ryan Sim <ryansim@usc.edu>
2022-04-14 15:50:45 -0700

    Update GreenHouse.c

--- Main Code/GreenHouse.c
+++ Main Code/GreenHouse.c
@@ -10,8 +10,14 @@ int main(void) {
   lcd_moveto(1, 3);
   lcd_writedata('c');
 
+  
+    unsigned char status ;
+    unsigned char abuf = {0 x01 , 0 x20 };
+    unsigned char rbuf [32];
+    
+    status = i2c_io (0 x62 , abuf , 2 , NULL , 0 , rbuf , 32);
+  
   while(1){
-
 
   }
 
