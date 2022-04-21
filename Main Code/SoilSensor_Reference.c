7bb569d27477e42750c3b48dca2b18595827d36f
Ryan Sim <ryansim@usc.edu>
2022-04-21 15:03:59 -0700

    Update SoilSensor_Reference.c

--- Main Code/SoilSensor_Reference.c
+++ Main Code/SoilSensor_Reference.c
@@ -11,7 +11,7 @@
  *    @param  stop Whether to send an I2C STOP signal on read
  *    @return True if read was successful, otherwise false.
  */
-bool Adafruit_I2CDevice::read(uint8_t *buffer, size_t len, bool stop) {
+bool read(uint8_t *buffer, size_t len, bool stop) {
   size_t pos = 0;
   while (pos < len) {
     size_t read_len =
@@ -33,9 +33,9 @@ bool Adafruit_I2CDevice::read(uint8_t *buffer, size_t len, bool stop) {
  *NOTE: not all seesaw firmwares have the temperature sensor enabled.
  *  @return     Temperature in degrees Celsius as a floating point value.
  ****************************************************************************************/
-float Adafruit_seesaw::getTemp() {
+float getTemp() {
   uint8_t buf[4];
-  this->read(SEESAW_STATUS_BASE, SEESAW_STATUS_TEMP, buf, 4, 1000);
+  read(SEESAW_STATUS_BASE, SEESAW_STATUS_TEMP, buf, 4, 1000);
   int32_t ret = ((uint32_t)buf[0] << 24) | ((uint32_t)buf[1] << 16) |
                 ((uint32_t)buf[2] << 8) | (uint32_t)buf[3];
   return (1.0 / (1UL << 16)) * ret;
@@ -51,13 +51,13 @@ float Adafruit_seesaw::getTemp() {
  *
  *  @return     the analog value. This is an integer between 0 and 1023
  ****************************************************************************/
-uint16_t Adafruit_seesaw::touchRead(uint8_t pin) {
+uint16_t touchRead(uint8_t pin) {
   uint8_t buf[2];
   uint8_t p = pin;
   uint16_t ret = 65535;
 
   for (uint8_t retry = 0; retry < 5; retry++) {
-    if (this->read(SEESAW_TOUCH_BASE, SEESAW_TOUCH_CHANNEL_OFFSET + p, buf, 2,
+    if (read(SEESAW_TOUCH_BASE, SEESAW_TOUCH_CHANNEL_OFFSET + p, buf, 2,
                    3000 + retry * 1000)) {
       ret = ((uint16_t)buf[0] << 8) | buf[1];
       break;
