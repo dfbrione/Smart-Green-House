a5c8b2cfb506e9362ad80d21f82d308a46672e46
Daniel Briones <dfbrione@usc.edu>
2022-04-26 15:24:26 -0700

    commit1

--- Main Code/GreenHouse.c
+++ Main Code/GreenHouse.c
@@ -1,24 +1,57 @@
+#include "Greenhouse.h"
 #include "Parallel_LCD_4b.h"
+#include "sensirion_i2c_hal.h"
+#include <util/delay.h>
+#include "i2c.h"
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+
+
+
+void splash(){
+  lcd_writecommand(1);
+  lcd_moveto(0,0);
+  lcd_stringout("Greenhouse");
+  _delay_ms(1000);
+}
+
 
 int main(void) {
 
   lcd_init();
+  i2c_init(BDIV);
+  splash(); //Display splash screen on LCD for 1 second
   //lcd_writecommand(1); //Clear LCD
   
-  
- 
-  lcd_moveto(1, 3);
-  lcd_writedata('c');
 
   
-    unsigned char status ;
-    unsigned char abuf = {0 x01 , 0 x20 };
+    unsigned char status;
+    unsigned char abuf = {'0x01'};
     unsigned char rbuf [32];
-    
-    status = i2c_io (0 x62 , abuf , 2 , NULL , 0 , rbuf , 32);
-  
-  while(1){
+    unsigned char wbuf [2];
 
-  }
+    sensirion_i2c_hal_init();
+
+    
+    
+    //status = i2c_io(0xD0, abuf, 1, NULL, 0, rbuf, 32);
+    //status = i2c_io(0xC5, abuf, 1, NULL, 50, rbuf, 32);
+    //status = i2c_io(0xC4, NULL, 0, wbuf, 2, NULL, 0);
+
+  
+/*
+   A typical write with a 2-byte address is done with
+
+    i2c_io(0xA0, abuf, 2, wbuf, 50, NULL, 0);
+
+A typical read with a 1-byte address is done with
+
+    i2c_io(0xD0, abuf, 1, NULL, 0, rbuf, 20);
+
+*/
+
 
 }
